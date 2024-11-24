8cdbde59dcdc3337fb9d5bcec0723f2a07756c14
Himansha Fernando <114354379+Himafdo1003@users.noreply.github.com>
2024-11-25 02:29:13 +0530

    Added new files

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,16 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter } from 'react-router-dom';
+import StoreContextProvider from './content/StoreContext.jsx';
 
 const root = createRoot(document.getElementById('root')); // Create the root element
 root.render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+    <StoreContextProvider>
+    <App />
+    </StoreContextProvider>
+      
     </BrowserRouter>
   </StrictMode>
 );
